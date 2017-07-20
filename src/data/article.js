d5fe75d1ee8cb304ede56431519902a86d1c146b
limin <limin@lianjia.com>
2017-07-20 16:30:55 +0800

    增加routes4.0

--- src/data/article.js
+++ src/data/article.js
@@ -7,6 +7,9 @@
 			"title":"从输入URL到浏览器显示页面发生了什么","path":"/Http","description":"简单介绍了从输入URL到浏览器显示页面发生了什么","createTime":"2017/07/10 16:34:25","component":()=>System.import('article/Http.md')
 		},
 		{
+			"title":"Router 4.0的用法","path":"/Router4","description":"React_router升级到4.0后的变化","createTime":"2017/07/20 12:34:25","component":()=>System.import('article/Router4.md')
+		},
+		{
 			"title":"常用的正则表达式","path":"/Util","description":"简单介绍了项目中比较常用的正则表达式","createTime":"2017/05/10 16:34:25","component":()=>System.import('article/Util.md')
 		}]
 	
